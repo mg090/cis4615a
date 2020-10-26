f0be16cd478224cc9f63f9d70f77dd75151bd173
mg090 <73413044+mg090@users.noreply.github.com>
2020-10-25 22:20:39 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -3,3 +3,8 @@ if (loginSuccessful) {
 } else {
   logger.severe("User login failed for: " + sanitizeUser(username));
 }
+
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
+}
